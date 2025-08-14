cd5b3284cd77ebb7d10f80d1d655a1ee4af98169
2-Chengs <ivan.cheng011099@gmail.com>
2025-08-13 23:15:38 -0700

    chore: add Facebook domain verification meta tag to <head> of root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,6 +60,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="facebook-domain-verification" content="2czgf26x1grjm2zjjtqsbw5czyovti" />
         {/* Web Vitals Monitoring */}
         <script
           defer
